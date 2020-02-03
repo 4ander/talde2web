f0f3d4bfb0521f8b3fb89624ab34cf4909e8a883
4ander <go.za.an@gmail.com>
2020-02-03 13:46:22 +0100

    web horria bukatuta, responsive egin beharra dago

---
+++ script/formularioa.js
@@ -0,0 +1,71 @@
+function textuaAldatu() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua").style.background = "yellow";
+}
+function textuaAldatu1() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua1").style.background = "yellow";
+}
+function textuaAldatu2() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua2").style.background = "yellow";
+}
+function textuaAldatu3() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua3").style.background = "yellow";
+}
+function textuaAldatu4() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua4").style.background = "yellow";
+}
+function textuaAldatu5() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua5").style.background = "yellow";
+}
+function textuaAldatu6() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua6").style.background = "yellow";
+}
+function textuaAldatu7() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua7").style.background = "yellow";
+}
+function textuaAldatu8() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua8").style.background = "yellow";
+}
+function textuaAldatu9() {
+  // Focus = Changes the background color of input to yellow
+  document.getElementById("textua9").style.background = "yellow";
+}
+
+function blurFunction() {
+  document.getElementById("textua").style.background = "white";
+}
+function blurFunction1() {
+  document.getElementById("textua1").style.background = "white";
+}
+function blurFunction2() {
+  document.getElementById("textua2").style.background = "white";
+}
+function blurFunction3() {
+  document.getElementById("textua3").style.background = "white";
+}
+function blurFunction4() {
+  document.getElementById("textua4").style.background = "white";
+}
+function blurFunction5() {
+  document.getElementById("textua5").style.background = "white";
+}
+function blurFunction6() {
+  document.getElementById("textua6").style.background = "white";
+}
+function blurFunction7() {
+  document.getElementById("textua7").style.background = "white";
+}
+function blurFunction8() {
+  document.getElementById("textua8").style.background = "white";
+}
+function blurFunction9() {
+  document.getElementById("textua9").style.background = "white";
+}
